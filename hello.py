a189393831f6141a446d9cf25dbe7a883f29fa52
jesusmb230795 <jesusmiranda2307@gmail.com>
2022-12-15 21:24:37 -0600

    other fix

--- hello.py
+++ hello.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
-def add(
-    x:float,
-    y:float
-    ):
-    return int(x+y)
+
+def add(x, y):
+    return int(x + y)
 
 
-print(add(1,1))
-# var=1
-# var=var
+print(add(1, 1))
+#var=1
+#var=var
